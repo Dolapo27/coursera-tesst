09a7b994be01102b7d16b49726a791b923b0f87f
Oladipupo Dolapo <70181857+Dolapo27@users.noreply.github.com>
2020-11-26 17:21:46 +0100

    Add files via upload

---
+++ assignmentweek4/js/script.js
@@ -0,0 +1,29 @@
+//var multiple = 5;
+//for (var i = 0; i < 10; i=i+1);{
+//    multiple = multiple * i;
+//}
+//console.log("the multiples of 5 is " + multiple);
+
+
+//function givehimthe(game){
+  //  game = game || "Anyhow!"
+    //console.log("Take the stuff with a " + game);
+//}
+
+
+//givehimthe("pad");
+//givehimthe("headset");
+//givehimthe();
+
+
+
+var company = {
+    name:"Facebook",
+    ceo:{
+        firstname:"Mark",
+        favcolor:"blue"
+    },
+    stock: 110
+};
+
+console.log(company)
